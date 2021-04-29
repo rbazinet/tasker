4c3985ffffe7b3065774799741cea4fb38dcc355
Rob Bazinet <rbazinet@stillriversoftware.com>
2021-04-29 10:58:28 -0400

    Add ability to create lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,9 +7,28 @@ class ListsController < ApplicationController
   end
 
   def new
-    cable_ready[ListsChannel].console_log(
-      message: "Hello from Rails"
+    cable_ready[ListsChannel].insert_adjacent_html(
+      selector: "#lists",
+      html: render_to_string(partial: "lists/form", locals: { list: List.new })
     )
     cable_ready.broadcast_to(current_user)
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      cable_ready[ListsChannel].outer_html(
+        selector: "#new-list",
+        html: render_to_string(@list)
+      )
+      cable_ready.broadcast_to(current_user)
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'lists#index'
-  resources :lists, only: [:new]
+  resources :lists, only: [:new, :create]
 end
